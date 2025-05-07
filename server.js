1f21709788523c1af7fcb51f1b36dbe616bb8556
Arianah255 <arohenesrequena@gmail.com>
2025-05-06 22:52:22 -0500

    server2

--- server.js
+++ server.js
@@ -24,11 +24,8 @@ app.get("/", (req, res) => {
 });
 
 // Connect to MongoDB and start server
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(process.env.MONGODB_URI)
+
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => {
